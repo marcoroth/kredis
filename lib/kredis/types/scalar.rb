267b738b0a275a066edaad5a57a56db64ec6ddba
Marco Roth <marco.roth@intergga.ch>
2022-03-08 00:58:00 +0200

    Default Values

--- lib/kredis/types/scalar.rb
+++ lib/kredis/types/scalar.rb
@@ -3,6 +3,14 @@ class Kredis::Types::Scalar < Kredis::Types::Proxying
 
   attr_accessor :typed, :default, :expires_in
 
+  def initialize(...)
+    super
+
+    if !exists? && default.present?
+      self.value = default_value
+    end
+  end
+
   def value=(value)
     set type_to_string(value, typed), ex: expires_in
   end
@@ -11,14 +19,18 @@ class Kredis::Types::Scalar < Kredis::Types::Proxying
     value_after_casting = string_to_type(get, typed)
 
     if value_after_casting.nil?
-      default
+      default_value
     else
       value_after_casting
     end
   end
 
+  def default_value
+    default.is_a?(Proc) ? default.call : default
+  end
+
   def to_s
-    get || default&.to_s
+    get || default_value&.to_s
   end
 
   def assigned?
